f63aba51697f1c55905738b7babbd5e9bb941626
yanglimou <2531463756@qq.com>
2017-09-13 09:50:18 +0800

    mq发送测试

---
+++ crawlerNews.china/src/test/java/com/zdjy/bigdata/logistic_warehouse/crawlerNews/china/test/MQSendTest.java
@@ -0,0 +1,21 @@
+package com.zdjy.bigdata.logistic_warehouse.crawlerNews.china.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MQSendTest {
+	@Autowired
+	private AmqpTemplate rabbitTemplate;
+	@Test
+	public void test001(){
+		//第一个参数是queue
+		//第二个参数是内容
+		rabbitTemplate.convertAndSend("news","asd");
+	}
+	
+}
